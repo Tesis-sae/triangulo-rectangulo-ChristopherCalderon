8f500475c9568c78bf4f8a9081b7528322e3a868
ChrisCalderon <62729165+ChristopherCalderon@users.noreply.github.com>
2025-05-03 17:04:08 -0600

    Update ejercicio.cpp

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -3,9 +3,24 @@
 using namespace std;
 
 void imprimirTriangulo(int altura) {
+    for (int i = 1; i <= altura; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
 
 int main() {
+    // Declaración de variables
+    int h1, h2, h3;
 
+    // Pedir los valores al usuario
+    cin >> h1 >> h2 >> h3;
+
+    // Llamar la funcion
+    imprimirTriangulo(h1);
+    imprimirTriangulo(h2);
+    imprimirTriangulo(h3);
     return 0;
 }
